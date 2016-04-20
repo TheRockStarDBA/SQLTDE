4ca424a6f72dae1a9b4a7b10850f3c4417235570
scotta01 <scotta01@gmail.com>
2016-04-20 14:10:00 +0100

    Initial Commit

---
+++ SQLTDE.Tests.ps1
@@ -0,0 +1,73 @@
+﻿Import-Module $PSScriptRoot\SQLTDE.psm1
+
+
+InModuleScope SQLTDE{
+
+    Describe "Module Imported"{
+        It "Exports SQLTDE Fuctions"{
+            Get-Command "New-SQLMasterKey" | Should Be $true
+            Get-Command "New-SQLServerCertificate" | Should Be $true
+            Get-Command "Backup-SQLServerCertificate" | Should Be $true
+            Get-Command "Set-SQLDatabaseEncryption" | Should Be $true
+        }
+    }
+    Describe "New-SQLMasterKey" {
+        Mock "Invoke-Sqlcmd"{
+                "Master Key Created"   
+        }
+        It "Imports the SQLPS Module if it is not already"{
+            New-SQLMAsterKey -SQLInstance localhost -EncryptionPassword (ConvertTo-SecureString "Password" -AsPlainText -Force)
+            Get-Module SQLPS | Should Be $true
+        }
+        It "Creates a key in the master database"{
+            New-SQLMasterKey -SQLInstance localhost -EncryptionPassword (ConvertTo-SecureString "Password" -AsPlainText -Force)
+            Assert-MockCalled Invoke-Sqlcmd -Exactly 1 -Scope It  
+        }
+    }
+    
+    Describe "New-SQLServerCertificate"{
+        Mock "Invoke-Sqlcmd"{
+            "SQL Cert Created"   
+        }
+        It "Imports the SQLPS Module if it is not already"{
+            New-SQLServerCertificate -SQLInstance localhost -Subject "Server Name"
+            Get-Module SQLPS | Should Be $true
+        }
+        It "Creates a SQL Server Certificate"{
+            New-SQLServerCertificate -SQLInstance localhost -Subject "Server Name"
+            Assert-MockCalled Invoke-Sqlcmd -Exactly 1 -Scope It  
+        }
+    }
+    
+    Describe "Backup-SQLServerCertificate"{
+        New-Item C:\localhost_key -ItemType File
+        New-Item C:\localhost_cert -ItemType File
+
+        Mock "Invoke-Sqlcmd"{
+            "Cert Backed up"   
+        }
+        It "Imports the SQLPS Module if it is not already"{
+            Backup-SQLServerCertificate -SQLInstance localhost -EncryptionPassword (ConvertTo-SecureString "Password" -AsPlainText -Force) -Path TestDrive:\
+            Get-Module SQLPS | Should Be $true
+        }
+        It "Copies the SQL Key to Path defined"{
+            Backup-SQLServerCertificate -SQLInstance localhost -EncryptionPassword (ConvertTo-SecureString "Password" -AsPlainText -Force) -Path TestDrive:\
+            (((ls TestDrive:\localhost*).Count) -eq 2) | Should Be $true
+            del C:\localhost_cert
+            del C:\localhost_key
+        }
+    }
+    Describe "Set-SQLDatabaseEncryption"{
+        Mock "Invoke-Sqlcmd"{
+            "Database Encrypted"   
+        }
+        It "Imports the SQLPS Module if it is not already"{
+            Set-SQLDatabaseEncryption -SQLInstance localhost -Database test
+            Get-Module SQLPS | Should Be $true
+        }
+        It "Turn on SQL database encryption"{
+            Set-SQLDatabaseEncryption -SQLInstance localhost -Database test
+            Assert-MockCalled Invoke-Sqlcmd -Exactly 2 -Scope It  
+        }
+    }
+}
---
+++ SQLTDE.psm1
@@ -0,0 +1,261 @@
+function New-SQLMasterKey
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    param
+    (
+        
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true,Position=0)]
+        $SQLInstance,
+		[Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true,Position=0)]
+		[securestring]
+        $EncryptionPassword = (Read-Host -AsSecureString -Prompt "Input password for encrypting the Master Key")
+	)
+        Begin
+        {
+			Import-SQLModule
+			$masterDB = "master" 
+        }
+
+        Process
+        {
+			$MASTER_KEY_QUERY = "CREATE MASTER KEY ENCRYPTION BY PASSWORD = '{0}';" -f (Show-Password -EncryptedPassword $EncryptionPassword)
+			try
+			{
+				Invoke-Sqlcmd -query $MASTER_KEY_QUERY -ServerInstance $SQLInstance -Database $masterDB
+				Write-Verbose "Created Master Key for instance $SQLInstance"
+			}
+			catch [System.Exception]
+			{
+				Write-Error "Error creating Master Key"
+			}
+			finally
+			{
+				
+			}
+			
+        }
+
+        End
+        {
+			$EncryptionPassword = $null
+        }
+}
+
+function New-SQLServerCertificate
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    param
+    (
+        
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true,Position=0)]
+        $SQLInstance,
+		[Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true,Position=1)]
+        $Subject
+	)
+        Begin
+        {
+			Import-SQLModule
+			$masterDB = "master" 
+			$splitName = ($SQLInstance -split "\\")
+			$splitname = $splitname[$splitName.Count-1] -replace "-",""
+			
+        }
+
+        Process
+        {
+			try
+			{
+				$SQL_SERVER_CERT_QUERY = "CREATE CERTIFICATE {0}_Cert WITH SUBJECT = '{1}'" -f $splitname,$Subject
+				Invoke-Sqlcmd -query $SQL_SERVER_CERT_QUERY -ServerInstance $SQLInstance -Database $masterDB 
+				Write-Verbose "Certificate Created Successfully"
+			}
+			catch [System.Exception]
+			{
+				Write-Error "Failed to create certificate"
+			}
+			finally
+			{
+
+			}
+
+        }
+
+        End
+        {
+
+        }
+}
+
+function Backup-SQLServerCertificate
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    param
+    (
+        
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true,Position=0)]
+        $SQLInstance,
+		[Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true,Position=1)]
+		[securestring]
+        $EncryptionPassword = (Read-Host -AsSecureString -Prompt "Input password for encrypting the Master Key"),
+		[Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true,Position=2)]
+        $Path
+	)
+        Begin
+        {
+			Import-SQLModule
+			$masterDB = "master" 
+			$SQLServer = new-object ('Microsoft.SqlServer.Management.Smo.Server') $SQLInstance
+			$source = $null
+			if($SQLServer.DefaultFile -eq $null){
+				$source = $SQLServer.MasterDBPath
+			}
+			else{
+				$source = $SQLServer.DefaultFile
+			}
+			$splitName = ($SQLInstance -split "\\")
+			$splitname = $splitname[$splitName.Count-1] -replace "-",""
+        }
+
+        Process
+        {
+
+			$SQL_SERVER_CERT_BACKUP_QUERY = "BACKUP CERTIFICATE {0}_Cert TO FILE = '{0}_Cert' WITH PRIVATE KEY (FILE = '{0}_Key', ENCRYPTION BY PASSWORD = '{1}')" -f $splitname,(Show-Password -EncryptedPassword $EncryptionPassword)
+			Invoke-Sqlcmd -query $SQL_SERVER_CERT_BACKUP_QUERY -ServerInstance $SQLInstance -Database $masterDB
+			if($Path -eq $null){
+				Write-Verbose "Files saved to Default SQL Data Directory, no further backups made"
+			}
+			else{
+				Copy "$source\$($splitname)_Cert" $Path
+				Copy "$source\$($splitname)_Key" $Path
+			}
+        }
+
+        End
+        {
+			$EncryptionPassword= $null
+        }
+}
+
+function Set-SQLDatabaseEncryption
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    param
+    (
+        
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true,Position=0)]
+        $SQLInstance,
+		[Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true,Position=1)]
+        $Database
+	)
+        Begin
+        {
+			Import-SQLModule
+			$splitName = ($SQLInstance -split "\\")
+			$splitname = $splitname[$splitName.Count-1] -replace "-",""
+			
+        }
+
+        Process
+        {
+			try
+			{
+				$SQL_DB_KEY_QUERY = "CREATE DATABASE ENCRYPTION KEY WITH ALGORITHM = AES_128 ENCRYPTION BY SERVER CERTIFICATE {0}_Cert" -f $splitname
+				Invoke-Sqlcmd -query $SQL_DB_Key_QUERY -ServerInstance $SQLInstance -Database $Database 
+				Write-Verbose "Database Key Created Successfully"
+			}
+			catch [System.Exception]
+			{
+				Write-Error "Failed to create certificate"
+			}
+			finally
+			{
+
+			}
+			
+			try {
+				$SQL_DB_ENCRYPT_QUERY = "ALTER DATABASE {0} SET ENCRYPTION ON" -f $Database
+				Invoke-Sqlcmd -query $SQL_DB_ENCRYPT_QUERY -ServerInstance $SQLInstance -Database $Database 
+				Write-Verbose "Database Key Created Successfully"
+				
+			}
+			catch [System.Exception] {
+				Write-Error "Failed to encrypt database"
+			}
+			finally {
+				
+			}
+
+        }
+
+        End
+        {
+
+        }
+}
+
+function Show-Password
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    param
+    (
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true,Position=0)]
+        [securestring]
+		$EncryptedPassword
+	)
+        Begin
+        {
+			     
+        }
+
+        Process
+        {
+			$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($EncryptedPassword)
+			$PlainPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+        }
+
+        End
+        {
+			return $PlainPassword
+        }
+}
+
+function Import-SQLModule
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    param
+    (
+        
+	)
+        Begin
+        {
+			     
+        }
+
+        Process
+        {
+			if (-not(Get-Module -name 'SQLPS')) 
+			{
+				if (Get-Module -ListAvailable | Where-Object {$_.Name -eq 'SQLPS' }) {
+					Push-Location 
+                    Import-Module -Name 'SQLPS' -DisableNameChecking | Out-Null
+					Pop-Location 
+				}
+				else{
+					Write-Error " Could not find SQL Powershell module. Check it is installed and try again"
+				}
+            }
+        }
+
+        End
+        {
+		
+        }
+}
+
+#Export-ModuleMember -Function "New-SQLMasterKey","New-SQLServerCertificate","Backup-SQLServerCertificate","Set-SQLDatabaseEncryption"
